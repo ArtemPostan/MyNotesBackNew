1c09683f7787ede7c2884af5ebc6e09f571821ae
Artyom <arpost89@gmail.com>
2026-03-30 15:01:32 +0500

    Hide secrets

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 # Stage 1: Build
-# Используем зеркало mirror.gcr.io, чтобы избежать ошибки EOF
 FROM mirror.gcr.io/library/maven:3.9.6-eclipse-temurin-17-alpine AS build
 WORKDIR /app
 COPY pom.xml .
@@ -8,15 +7,15 @@ COPY src ./src
 RUN mvn clean package -DskipTests
 
 # Stage 2: Run
-# Здесь тоже используем зеркало
 FROM mirror.gcr.io/library/eclipse-temurin:17-jre-alpine
 WORKDIR /app
 
-# Копируем результат сборки
 COPY --from=build /app/target/*.jar app.jar
 
-# Настройки для YDB (если используешь метаданные облака)
+# Настройки для YDB
 ENV USE_METADATA=true
+
+ENV SPRING_PROFILES_ACTIVE=prod
 
 EXPOSE 8080
 
--- src/main/java/postanogov/dev/mynotesnew/config/JwtUtils.java
+++ src/main/java/postanogov/dev/mynotesnew/config/JwtUtils.java
@@ -2,26 +2,35 @@ package postanogov.dev.mynotesnew.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtUtils {
-    private final String jwtSecret = "mySecretKeyForMyNotesProject1234567890_VERY_LONG_STRING";
-    private final int jwtExpirationMs = 86400000;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
-    // В 0.12.5 используем SecretKey
-    private final SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(key) // Алгоритм определится автоматически по длине ключа
+                .signWith(key)
                 .compact();
     }
 
